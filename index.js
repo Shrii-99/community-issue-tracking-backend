adf60a50b0b38a417722a0f48d21521186a617a1
Om Bhor <ombhor617@gmail.com>
2025-09-02 14:00:12 +0530

    Connection db , security middlwares , global middlwares , cors configuration

--- index.js
+++ index.js
@@ -1,12 +1,80 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
-import express from "express"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import limiter from "./src/util/rateLimitter.js";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import morgan from "morgan";
+import xss from "xss-clean";
+import connectDB from "./src/database/db.js";
 
-app.get("/" , (req , res) => {
-    res.send("Hii from server")
+const port = process.env.PORT || 3000;
+const app = express();
+
+// Security Middleware
+app.use(helmet()); // Set security HTTP headers
+// app.use(mongoSanitize()); // Data sanitization against NoSQL query injection
+// app.use(xss()); // Data sanitization against XSS
+app.use(hpp()); // Prevent HTTP Parameter Pollution
+app.use("/api", limiter); // Apply rate limiting to all routes
+
+// Logging Middleware only enable when the env is development not for production
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Body Parser Middleware
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "kb" }));
+app.use(cookieParser());
+
+//cors configuration
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
+  })
+);
+
+
+//Api Routes
+app.get('/api/' , (req , res) => {
+  res.send("Hii from server")
 })
 
-const port = process.env.PORT;
-app.listen(port , () =>{
-    console.log(`Server is running on http://localhost:${port}`);
-} )
+
+
+//404 handler
+app.use((req, res) => {
+  res.status(404).json({ status: 404, message: "Route not found" });
+});
+
+// Global Error Handler.
+app.use((err, req, res, next) => {
+  console.error(err);
+  return res.status(err.statusCode || 500).json({
+    status: "error",
+    message: err.message || "Internal server error",
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  });
+});
+
+app.listen(port, () => {
+  connectDB();
+  console.log(
+    `Server is running on http://localhost:${port} in ${process.env.NODE_ENV} mode`
+  );
+});
