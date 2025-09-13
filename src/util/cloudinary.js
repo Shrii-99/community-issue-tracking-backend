d17b4dd967a53e8d5aad18e35660bf88f021baf0
Om Bhor <ombhor617@gmail.com>
2025-09-13 15:22:52 +0530

    Cloudinary setup ,and multer

--- src/util/cloudinary.js
+++ src/util/cloudinary.js
@@ -1,3 +1,28 @@
-import cloudinary from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
 
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+});
 
+export const uploadMedia = async (file) => {
+  try {
+    const uploadResponse = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+
+    return uploadResponse;
+  } catch (error) {
+    console.log(error, "Cloudinary upload fail");
+  }
+};
+export const deleteMediaFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.log(error, "Cloudinary delete fail");
+  }
+};
